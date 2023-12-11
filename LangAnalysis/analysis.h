1dd9f0cd3d2e08faa8cd078af605ad5a11967ece
BobHuangC <huang2003bo@sjtu.edu.cn>
2023-12-11 16:06:47 +0800

    fix output bug

--- LangAnalysis/analysis.h
+++ LangAnalysis/analysis.h
@@ -1,3 +1,11 @@
+#ifndef __ANALYSIS_H__
+#define __ANALYSIS_H__
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdio.h>
 #include "lang.h"
 #include "lexer.h"
@@ -8,3 +16,9 @@
 // file_name is the name of the file to be parsed
 // ret is the string to be stored and returned
 void get_syntax_tree(char* file_name, char* ret);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
