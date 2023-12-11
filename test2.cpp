c8a0e8b99e9e1b2303e2af1a7de1f490a63b2e38
BobHuangC <huang2003bo@sjtu.edu.cn>
2023-12-11 11:37:49 +0800

    add the structure of the whole project

---
+++ test2.cpp
@@ -0,0 +1,22 @@
+#include "PreProcess/input.h"
+#include "LangAnalysis/analysis.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+#include <sstream>
+#include <vector>
+#include <string>
+
+
+
+int main(int argc, char* argv[]) {
+    char ret[3000];
+    std::string filename = "LangAnalysis/sample_src00.jtl";
+    char* file = new char[filename.length() + 1];
+    strcpy(file, filename.c_str());
+    get_syntax_tree(file, ret);
+    std::cout << ret << std::endl;
+   
+    return 0;
+}
